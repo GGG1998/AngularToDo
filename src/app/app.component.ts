8397b6135d1843408c18cbd7cee5b76bc9ded9f3
GGG1998 <gabrys19@onet.eu>
2017-06-21 17:24:03 +0200

    add body style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @Input() name_item: string;
+  list: string[]=[];
+
+  onAddItem() {
+    this.list.push(this.name_item);
+  }
+
+  onRemoveItem(val: number) {
+    this.list.splice(val,1);
+  }
 }
